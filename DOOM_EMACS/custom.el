ebabacb0d1eb4809ce603cdad71894bb55c5bf1f
marceloexc <marceloexc@tutanota.de>
2024-04-29 21:31:05 -0500

    Initial Commit

---
+++ DOOM_EMACS/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7776ba149258df15039b1f0aba4b180d95069b2589bc7d6570a833f05fdf7b6d" "f42ea77c304f98f13ce4ed3b7b84d4a96c69e651757185ba26aa7059d3219051" "159a29ab0ec5ba4e2811eddd9756aa779b23467723dcbdd223929fbf2dde8954" "d5c2ce5d9924c06bfe65a18e37d63dc75862a1f3034a893563e626dc80a3fb13" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290" "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("/Users/marceloexc/Documents/UTRGV/2024Spring/school_agenda.org"))
+ '(package-selected-packages
+   '(org-ros doom-themes org-download preview-dvisvgm vertico ligature org-modern latex-extra good-scroll cdlatex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:height 1.1))))
+ '(org-level-2 ((t (:height 1.05))))
+ '(org-level-3 ((t (:size 17)))))
